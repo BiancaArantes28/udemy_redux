0b830e1d48d9616f7c71371e69d2b7b2d414d7a6
Bianca Arantes <bianca.santos-ext@my-rio.de>
2019-03-28 17:00:05 -0300

    First commit

---
+++ src/home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+
+const Home = (props) => (
+    <div className="page-container">
+        <div className="new-list-container">
+            <Card className="card">
+                <CardActionArea className="card-action-area">
+                    <CardContent className="card-content">
+                        <div>
+                            <p className="title">Adicionar Novas Listas!</p>
+                        </div>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </div>
+
+    </div>
+)
+
+export default Home
